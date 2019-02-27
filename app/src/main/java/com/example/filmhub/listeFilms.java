011a6d2e0625fb1d036934bc6864f4caaa4c9fc7
Angel Aguilera <albertoangelaguilera@gmail.com>
2019-02-27 02:57:57 -0600

    Layout initial de la liste des films ajouté. C'est aussi le backup du projet avant de commencer a faire le "merge" avec les pull requests des autres.

---
+++ app/src/main/java/com/example/filmhub/listeFilms.java
@@ -0,0 +1,15 @@
+package com.example.filmhub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class listeFilms extends AppCompatActivity   {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_liste_films);
+
+    }
+}
