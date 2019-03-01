637478fe6c661486f93b10e4c55d3641d84bebd9
Tanguy Rosar <tanguy.rosar@hei.yncrea.fr>
2019-03-01 12:02:45 +0100

    Ajout d'activity : EditProfile avec son layout. + intent sur le bouton pour y accéder (+ajout des films sur la BDD)

--- app/src/main/java/com/example/filmhub/ProfileActivity.java
+++ app/src/main/java/com/example/filmhub/ProfileActivity.java
@@ -1,13 +1,32 @@
 package com.example.filmhub;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ProfileActivity extends AppCompatActivity {
+    public Button buttonEditProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        buttonEditProfile = (Button) findViewById(R.id.buttonEdit);
+
+        buttonEditProfile.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent editProfileIntent = new Intent(ProfileActivity.this, EditProfileActivity.class);
+                startActivity(editProfileIntent);
+            }
+        });
+
+
+
     }
+
 }
