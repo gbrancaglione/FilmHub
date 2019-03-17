1a59768868b76eacda8701e92cea0b6de7dcc917
Angel Aguilera <albertoangelaguilera@gmail.com>
2019-03-16 23:19:31 -0600

    Get ID in Profil Activity

--- app/src/main/java/com/example/filmhub/ProfileActivity.java
+++ app/src/main/java/com/example/filmhub/ProfileActivity.java
@@ -16,10 +16,13 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
@@ -39,7 +42,7 @@ public class ProfileActivity extends Fragment {
     private List<Reviews> userReviews;
     private FirebaseAuth mAuth;
     private View v;
-
+    private CollectionReference ref;
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstance) {
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         super.onCreate(savedInstance);
@@ -47,12 +50,28 @@ public class ProfileActivity extends Fragment {
         mAuth = FirebaseAuth.getInstance();
         nomUtilisateur = v.findViewById(R.id.nomUtilisateurProfil);
         nomUtilisateur.setText(mAuth.getCurrentUser().getEmail());
+        ref = db.collection("Utilisateurs");
+        ref.whereEqualTo("adresseEmail", mAuth.getCurrentUser().getEmail()).get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+            @Override
 
+                public void onSuccess (QuerySnapshot queryDocumentSnapshots){
+                String id = "";
+                for (QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots) {
+                        Log.d("holi", documentSnapshot.getId());
+                        id += "ID +" +documentSnapshot.getId();
+                        getReviews(id);
+                    }
 
+            }
 
-
-
+            });
         return v;
+
+    }
+    void getReviews(String document){
+        //Aqui ya obtenemos el ID del usuario :D
+        //nomUtilisateur.setText(data);
+
 
     }
 }
